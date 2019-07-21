e40e53ba327907fbaa3915a9488d924c1ac4e960
Jake Knecht <Ljknecht91@gmail.com>
2019-07-21 17:11:08 -0500

    update Review.js

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -5,7 +5,31 @@ import {connect} from 'react-redux';
 
 class Review extends Component {
 
+    // Establish component local state to be updated
+    // with data from input components!
+state = {
+    newSubmission ={
+        feelings:0,
+        understanding:0,
+        supported:0,
+        comments:'',
+        flagged: 'false',
+        date: '',
+    }
 
+}
+
+    render() {
+
+        return (
+           
+            <div className="mainReviewDiv">
+                
+            </div>
+            )
+    }
+         
+    
 
 }
 
