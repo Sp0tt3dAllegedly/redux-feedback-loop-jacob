d6101d7aeac5a5be9223e79ea3b4a7c2004e8cb1
Jake Knecht <Ljknecht91@gmail.com>
2019-07-22 01:45:18 -0500

    update thanks.js

--- src/components/Thanks/Thanks.js
+++ src/components/Thanks/Thanks.js
@@ -5,7 +5,19 @@ import {connect} from 'react-redux';
 
 class Thanks extends Component{
 
+    goHome =()=> {
+        this.props.history.push('/feelings');
+    }
 
+    render () {
+        return(
+            <>
+            <h1>Thanks for the feedback :)</h1>
+                <p>Remember to do your feedback every day!</p>
+            <button onClick = {this.goHome}>Home</button>
+            </>
+    )
+}
 
 }
 
